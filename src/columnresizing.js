d77154421c131aeaffe2ec17e0f06d37bcebdc4b
wkl007 <499657357@qq.com>
2021-12-29 14:28:10 +0800

    fix: repair generate decorations judgement

--- src/columnresizing.js
+++ src/columnresizing.js
@@ -212,8 +212,7 @@ function handleDecorations(state, cell) {
     // For positions that are have either a different cell or the end
     // of the table to their right, and either the top of the table or
     // a different cell above them, add a decoration
-    if ((col == map.width || map.map[index] != map.map[index + 1]) &&
-        (row == 0 || map.map[index - 1] != map.map[index - 1 - map.width])) {
+    if (col == map.width || map.map[index] != map.map[index + 1]) {
       let cellPos = map.map[index]
       let pos = start + cellPos + table.nodeAt(cellPos).nodeSize - 1
       let dom = document.createElement("div")
